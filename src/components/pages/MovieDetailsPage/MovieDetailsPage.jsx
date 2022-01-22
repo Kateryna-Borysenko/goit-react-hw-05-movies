938cf4e3a42a581f9cc7bfc78daa6f16c5e083ac
Kateryna-Borysenko <k.borysenko.kyiv@gmail.com>
2022-01-22 20:10:02 +0200

    Added styles to movie ditails

--- src/components/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/components/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,17 +1,32 @@
 import { fetchMovieById } from 'services/api';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'; //позволит вытащить id
-import s from './MovieDetailsPage.modulse.css';
+import s from './MovieDetailsPage.module.css';
 
 const MovieDetailsPage = () => {
-  const { movieId } = useParams();
+  const { movieId } = useParams(); //возвращает динамический прараметр //! :movieId
   const [movie, setMovie] = useState({});
 
   useEffect(() => {
     fetchMovieById(movieId).then(setMovie);
   }, [movieId]);
 
-  // console.log(movie);
+  // useEffect(() => {
+  //   const getMovie = async () => {
+  //     try {
+  //       const movie = await fetchMovieById(movieId);
+
+  //       setMovie({ ...movie });
+  //     } catch (error) {
+  //       console.log(error);
+  //     } finally {
+  //       console.log('Лоудер');
+  //     }
+  //   };
+  //   getMovie();
+  // }, [movieId]);
+
+  console.log(movie);
 
   const {
     original_name,
@@ -24,32 +39,29 @@ const MovieDetailsPage = () => {
   } = movie;
 
   return (
-    <div>
-      {vote_average}
-      {/* <button className="Button" type="button" onClick={onGoBack}>
-        ⬅ Go back
-      </button> */}
-
-      <div className={s.wrap}>
-        <img
-          className={s.image}
-          src={
-            poster_path
-              ? `https://image.tmdb.org/t/p/w500${poster_path}`
-              : 'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg'
-          }
-          alt={original_title}
-        />
-        <div className={s.contentWrap}>
-          <h3 className={s.title}>{original_title || original_name}</h3>
-          <span className={s.releaseData}>{release_date.slice(0, 4)}</span>
+    // <div className={s.contentWrap}>Test</div>
+    <div className={s.wrap}>
+      <h3 className={s.title}>{original_title || original_name}</h3>
+      <span className={s.releaseData}>{release_date.slice(0, 4)}</span>
+      <img
+        className={s.image}
+        src={
+          poster_path
+            ? `https://image.tmdb.org/t/p/w500${poster_path}`
+            : 'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg'
+        }
+        alt={original_title}
+      />
+      <div className={s.contentWrap}>
+        <div>
           <span className={s.score}>User Score : </span>
-          <span className={s.scoreValue}>{vote_average * 10} %</span>
+          <span className={s.scoreValue}> {vote_average * 10} %</span>
           <h4 className={s.subtitle}>Overview</h4>
           <p className={s.description}>{overview}</p>
-          <span className={s.genres}>Genres</span>
+          <h4 className={s.subtitle}>Genres</h4>
+          <hr />
           <p className={s.genreValues}>
-            {genres.map(genre => genre.name).join(', ')}
+            {genres.map(genre => genre.name).join(' | ')}
           </p>
         </div>
       </div>
