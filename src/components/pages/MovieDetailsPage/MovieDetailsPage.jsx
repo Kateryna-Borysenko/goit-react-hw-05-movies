f512e46c572d85f0376ba4dcc114962284a195a4
Kateryna <k.borysenko.kyiv@gmail.com>
2022-01-29 21:05:37 +0200

    Added read me file

--- src/components/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/components/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -47,67 +47,67 @@ const MovieDetailsPage = () => {
   } = movie;
 
   return (
-    // <div className={s.contentWrap}>Test</div>
+    <div className={s.contentWrap}>Test</div>
 
-    <>
-      {movie && (
-        <>
-          <div className={s.wrap}>
-            <button className={s.button} type="button" onClick={onGoBack}>
-              ⬅ Go back
-            </button>
-            <h3 className={s.title}>{original_title || original_name}</h3>
-            <span className={s.releaseData}>{release_date.slice(0, 4)}</span>
-            {poster_path && (
-              <img
-                className={s.image}
-                src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-                alt={original_title}
-              />
-            )}
-            <div className={s.contentWrap}>
-              <div>
-                <span className={s.score}>User Score : </span>
-                <span className={s.scoreValue}> {vote_average * 10} %</span>
-                <h4 className={s.subtitle}>Overview</h4>
-                <p className={s.description}>{overview}</p>
-                <h4 className={s.subtitle}>Genres</h4>
-                <hr />
-                <p className={s.genreValues}>
-                  {genres.map(genre => genre.name).join(' | ')}
-                </p>
-              </div>
-            </div>
-          </div>
-          <NavLink
-            className={s.link}
-            activeClassName={s.activeLink}
-            to={`${url}/cast`}
-          >
-            Cast
-          </NavLink>
-          <NavLink
-            className={s.link}
-            activeClassName={s.activeLink}
-            to={`${url}/reviews`}
-          >
-            Reviews
-          </NavLink>
-        </>
-      )}
-      {/* тут может быть loader */}
-      <Suspense fallback={<h2>Loading ...</h2>}>
-        <Switch>
-          <Route path={`${path}/cast`}>
-            <Cast movieId={movieId} />
-          </Route>
+    // <>
+    //   {movie && (
+    //     <>
+    //       <div className={s.wrap}>
+    //         <button className={s.button} type="button" onClick={onGoBack}>
+    //           ⬅ Go back
+    //         </button>
+    //         <h3 className={s.title}>{original_title || original_name}</h3>
+    //         <span className={s.releaseData}>{release_date.slice(0, 4)}</span>
+    //         {poster_path && (
+    //           <img
+    //             className={s.image}
+    //             src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+    //             alt={original_title}
+    //           />
+    //         )}
+    //         <div className={s.contentWrap}>
+    //           <div>
+    //             <span className={s.score}>User Score : </span>
+    //             <span className={s.scoreValue}> {vote_average * 10} %</span>
+    //             <h4 className={s.subtitle}>Overview</h4>
+    //             <p className={s.description}>{overview}</p>
+    //             <h4 className={s.subtitle}>Genres</h4>
+    //             <hr />
+    //             <p className={s.genreValues}>
+    //               {genres.map(genre => genre.name).join(' | ')}
+    //             </p>
+    //           </div>
+    //         </div>
+    //       </div>
+    //       <NavLink
+    //         className={s.link}
+    //         activeClassName={s.activeLink}
+    //         to={`${url}/cast`}
+    //       >
+    //         Cast
+    //       </NavLink>
+    //       <NavLink
+    //         className={s.link}
+    //         activeClassName={s.activeLink}
+    //         to={`${url}/reviews`}
+    //       >
+    //         Reviews
+    //       </NavLink>
+    //     </>
+    //   )}
 
-          <Route path={`${path}/reviews`}>
-            <Reviews movieId={movieId} />
-          </Route>
-        </Switch>
-      </Suspense>
-    </>
+    //   <Suspense fallback={<h2>Loading ...</h2>}>
+    //     <Switch>
+    //       <Route path={`${path}/cast`}>
+    //         <Cast movieId={movieId} />
+    //       </Route>
+
+    //       <Route path={`${path}/reviews`}>
+    //         <Reviews movieId={movieId} />
+    //       </Route>
+    //     </Switch>
+    //   </Suspense>
+    // </>
   );
 };
 
